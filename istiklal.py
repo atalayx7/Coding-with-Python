71a89f3fe38c10a3005962417beb41a2e30cadd0
Atalay <atalay.ergen@std.antalya.edu.tr>
2019-10-06 01:38:55 +0300

    Kim Milyoner Olmak İster - Milyonluk Soru
    Kim Milyoner Olmak İster'de sorulan milyonluk sorunun Python ile çözümü. 
    Soru: On kıtadan oluşan İstiklal Marşı'nın tamamında bu kelimelerden hangisi daha az geçer?
    A) Vatan    B) Kan    C) Toprak    D) Yurt

---
+++ istiklal.py
@@ -0,0 +1,27 @@
+import re
+
+with open('istiklal.txt', encoding='utf-8') as x:
+    firstList = []
+    # x=x.readlines()
+    for i in x.readlines():
+        firstList.append(i.strip().split())
+dictList = {}
+dictList2 = {'toprak': 0, 'vatan': 0, 'yurt': 0, 'kan': 0}
+for i in range(len(firstList)):
+    for j in firstList[i]:
+        j = re.sub(r'\W+', '', j).lower()
+        if 'toprak' in j or 'toprağ' in j:
+            dictList2['toprak'] += 1
+        elif 'vatan' in j:
+            dictList2['vatan'] += 1
+        elif 'yurt' in j or 'yurd' in j:
+            dictList2['yurt'] += 1
+        elif 'kan' in j:
+            dictList2['kan'] += 1
+
+        if not j in dictList:
+            dictList[j] = 1
+        else:
+            dictList[j] += 1
+# print(dictList)
+print(dictList2)
